9fb12b14cb8a93d6634274d364f897ad32b6cb54
thiagovasconcellos <thiagovasconcellos88@gmail.com>
2019-12-24 11:03:47 -0300

    new feature: forgot password

--- start/routes.js
+++ start/routes.js
@@ -3,3 +3,6 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.Store')
+Route.post('sessions', 'SessionController.Store')
+
+Route.post('passwords', 'ForgotPasswordController.Store')
